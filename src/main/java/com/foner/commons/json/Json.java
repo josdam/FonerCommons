34b55460d705044c7a177127052c2b122c0463ef
Josep Carbonell <josepdcs@gmail.com>
2018-12-03 17:06:23 +0100

    Json

--- src/main/java/com/foner/commons/json/Json.java
+++ src/main/java/com/foner/commons/json/Json.java
@@ -1,7 +1,10 @@
 package com.foner.commons.json;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -45,15 +48,15 @@ public class Json implements PooleableObject {
 	 * The init.
 	 */
 	private void init() {
+		JsonFactory jsonFactory = new JsonFactory();
+		jsonFactory.enable(JsonParser.Feature.ALLOW_COMMENTS);
 		mapper = new ObjectMapper();
+		mapper = new ObjectMapper(jsonFactory);
 		mapper.findAndRegisterModules();
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		// Ignore null values when writing json.
-		mapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
-		// Don't throw an exception when json has extra fields you are
-		// not serializing on. This is useful when you want to use a pojo
-		// for deserialization and only care about a portion of the json
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
+		mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
 
 		this.writer = mapper.writer();
 		this.prettyWriter = mapper.writerWithDefaultPrettyPrinter();
@@ -99,6 +102,56 @@ public class Json implements PooleableObject {
 			logger.error("Error deserializing the given json: ", ex);
 		} finally {
 			logger.info("Deserialize in " + (System.currentTimeMillis() - time) + " ms.");
+		}
+		return null;
+	}
+
+	/**
+	 * Deserializes JSON content from given JSON content String into given java type.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param jsonString
+	 *            the json string
+	 * @param valueTypeRef
+	 *            the value type reference
+	 * @return the type refer
+	 */
+	public <T> T deserialize(String jsonString, TypeReference<T> valueTypeRef) {
+		long time = System.currentTimeMillis();
+		try {
+			logger.debug("Deserializer Json Instance: " + this);
+			return mapper.readValue(jsonString, valueTypeRef);
+		} catch (IOException ex) {
+			logger.error("Error deserializing the given json: ", ex);
+		} finally {
+			logger.info("Deserialize in " + (System.currentTimeMillis() - time) + " ms.");
+		}
+		return null;
+	}
+
+	/**
+	 * Gets the object value.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param object
+	 *            the object
+	 * @param valueType
+	 *            the value type
+	 * @return the type refer
+	 */
+	public <T> T getObjectValue(Object object, Class<T> valueType) {
+		long time = System.currentTimeMillis();
+		try {
+			if (object == null) {
+				return null;
+			}
+			return mapper.convertValue(object, valueType);
+		} catch (Exception ex) {
+			logger.error("Error getting object from the given json: ", ex);
+		} finally {
+			logger.debug("Got object from JSON in " + (System.currentTimeMillis() - time) + " ms.");
 		}
 		return null;
 	}
